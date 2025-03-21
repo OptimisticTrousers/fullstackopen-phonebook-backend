627a57a2ea5e0eefeb68eeae7dd7d0e20b59f991
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-03-21 19:57:51 -0400

    Add CORS to the backend to allow all origins to make requests. Complete Exercise 3.9: Phonebook backend step 9.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
@@ -38,6 +39,8 @@ morgan.token("body", function (req, res) {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+
+app.use(cors());
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
