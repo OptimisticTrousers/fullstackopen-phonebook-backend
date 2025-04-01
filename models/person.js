9c3d63e303545de1ec230916d7d6e0df4a3e1c7c
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-04-01 04:20:40 -0400

    Complete Exercise 3.20: Phonebook database, step 8

--- models/person.js
+++ models/person.js
@@ -14,7 +14,11 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3 },
-  number: String,
+  number: {type: String, minLength: 8, validate: {
+    validator: function(v) {
+      return /\d{2,3}-\d{5,}/.test(v)
+    }
+  }},
 });
 
 personSchema.set("toJSON", {
